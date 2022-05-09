47792ba7774d94319a655f29b105abb8c1b414bd
foghill <ben@thefoghill.com>
2022-05-09 12:10:30 -0400

    commit tree 37c8ea6bfb8221af2878056d1910946491664b3d

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
-// Code Keypad Component Here
+// Code EyesOnMe Component Here
+import React from "react";
+
+function Keypad() {
+  function handleChange(event) {
+    event.preventDefault();
+    console.log(`Entering password...`);
+  }
+
+  return (
+    <>
+      <h1>Keypad</h1>
+      <input
+        type="password"
+        name="search"
+        onChange={handleChange}
+        placeholder="Enter password..."
+      />
+    </>
+  );
+}
+
+export default Keypad;
